be313424eded0e0aa27906c3c1eaba8c147823a7
Hibee-tech <ibrahimwale768@gmail.com>
2025-05-25 15:55:11 +0100

    Fixed loop implementation in 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,4 +1,11 @@
 const x = parseInt(process.argv[2]);
 
 // Check if x is a valid integer
-console.log(isNaN(x) || x < 1 ? "Missing number of occurrences" : Array(x).fill("C is fun").join("\n"));
+if (isNaN(x) || x < 1) {
+    console.log("Missing number of occurrences");
+} else {
+    // Use a loop to print "C is fun" x times
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
+}
