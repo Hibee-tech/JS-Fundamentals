0399e088e4ab52a73ecd4170f1c9d2067bf8fc6c
Hibee-tech <deanhorlar@gmail.com>
2025-05-25 15:30:21 +0100

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
-// Define an array of strings
 const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 // Loop through the array and print each element
 for (let i = 0; i < languages.length; i++) {
     console.log(languages[i]);
+}
